39b6b3658de7260f7e171040be99f5dd602b133c
Noah <noah@gmail.com>
2021-05-09 17:25:34 -0700

    adds a few patchs

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=12"
@@ -16,8 +17,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 #include "/home/noah/.cache/wal/colors-wal-dmenu.h"
 
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+ static unsigned int lines      = 3;
+ static unsigned int columns    = 9;
 
 /*
  * Characters not considered part of a word while deleting words
