78b03dda05f47ea3a46e63bef101ac68d928a296
Noah <noah@gmail.com>
2021-05-09 17:43:17 -0700

    update

--- config.h
+++ config.h
@@ -18,8 +18,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 #include "/home/noah/.cache/wal/colors-wal-dmenu.h"
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
- static unsigned int lines      = 3;
- static unsigned int columns    = 9;
+ static unsigned int lines      = 5;
+ static unsigned int columns    = 10;
 
 /*
  * Characters not considered part of a word while deleting words
